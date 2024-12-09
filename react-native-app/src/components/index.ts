a5145d6f999879a26da9681fcea3690bd2cdf827
Eliaslhl <eliaslahlouh@gmail.com>
2024-12-09 21:02:13 +0100

    CZEN-135 | feat: add template Connection

--- react-native-app/src/components/index.ts
+++ react-native-app/src/components/index.ts
@@ -1,4 +1,4 @@
 export { Button, Checkbox } from './atoms';
-export { Input } from './molecules';
+export { Input, VectorHeader, VectorBottom } from './molecules';
 export { Connection } from './organisms';
 export { ConnectionTemplate } from './templates';
