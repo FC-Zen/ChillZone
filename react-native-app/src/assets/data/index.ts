e945937caeda3ed9016afad133e98b4f9c7c4002
Héléna Chevalier <lna.chevalier@gmail.com>
2024-12-10 22:19:53 +0100

    CZEN-133 | fix : add index module

---
+++ react-native-app/src/assets/data/index.ts
@@ -0,0 +1,32 @@
+import commands from './commands.json';
+export { commands };
+
+import faq from './faq.json';
+export { faq };
+
+import maps from './maps.json';
+export { maps };
+
+import meals from './meals.json';
+export { meals };
+
+import menus from './menus.json';
+export { menus };
+
+import notifications from './notifications.json';
+export { notifications };
+
+import reservations from './reservations.json';
+export { reservations };
+
+import restauration_places from './restauration_places.json';
+export { restauration_places };
+
+import rooms from './rooms.json';
+export { rooms };
+
+import social_networks from './social_networks.json';
+export { social_networks };
+
+import user_test from './user_test.json';
+export { user_test };
--- react-native-app/src/assets/index.ts
+++ react-native-app/src/assets/index.ts
@@ -4,5 +4,8 @@ export { fr };
 import en from './en.json';
 export { en };
 
+export * as Data from './data';
+
 export * as Image from './Images';
 export { VectorHeader, VectorBottom } from './Images';
+
