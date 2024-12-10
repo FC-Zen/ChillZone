f831842275367d068832ef11434329a1ea1c5236
tahathierrysefoudine <tahathierry130@gmail.com>
2024-12-10 22:56:20 +0100

    feat : ajout notifications non fini

---
+++ react-native-app/src/components/organisms/Notification/NotificationList.tsx
@@ -0,0 +1,35 @@
+// src/components/organisms/NotificationList.tsx
+import React from 'react';
+import { FlatList } from 'react-native';
+import NotificationItem from '../../molecules/Notification/NotificationItem';
+
+interface Notification {
+  id: number;
+  title: string;
+  message: string;
+  time: string;
+}
+
+interface NotificationListProps {
+  notifications: Notification[];
+}
+
+const NotificationList: React.FC<NotificationListProps> = ({
+  notifications,
+}) => {
+  return (
+    <FlatList
+      data={notifications}
+      renderItem={({ item }) => (
+        <NotificationItem
+          title={item.title}
+          message={item.message}
+          time={item.time}
+        />
+      )}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
+};
+
+export default NotificationList;
