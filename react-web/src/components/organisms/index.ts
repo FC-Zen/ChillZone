66b16c4a1bcd62b89bb3a59e528a7e7f9d6c5472
Eliaslhl <eliaslahlouh@gmail.com>
2024-12-11 19:33:57 +0100

    CZEN-135 | feat: add organism web

---
+++ react-web/src/components/organisms/index.ts
@@ -0,0 +1,4 @@
+export { Connection } from './Connection';
+export { ForgotPassword } from './ForgotPassword';
+export { ForgotMdp } from './ForgotMdp';
+export { ResetPassword } from './ResetPassword';
