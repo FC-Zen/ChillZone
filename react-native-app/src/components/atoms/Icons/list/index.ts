076c0aef9514cb8c757b9241edf9fe4312e1b70a
tahathierrysefoudine <tahathierry130@gmail.com>
2024-12-12 10:45:53 +0100

    CZEN-139 | fix: pull branch

--- react-native-app/src/components/atoms/Icons/list/index.ts
+++ react-native-app/src/components/atoms/Icons/list/index.ts
@@ -1,8 +1,5 @@
 export { default as Lock } from './Lock';
 export { default as User } from './User';
-<<<<<<< HEAD
 export { default as Bell } from './Bell';
 
-=======
 export { default as Inbox } from './Inbox';
->>>>>>> 879dbcff550c53566b0fc529324b131c0f7ee84a
--- react-native-app/src/navigation/RootNavigator/RootNavigator.tsx
+++ react-native-app/src/navigation/RootNavigator/RootNavigator.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-<<<<<<< HEAD
 import { LoginScreen, NotificationScreen } from '@screens';
-=======
-import {
-  ForgotMdpScreen,
-  ForgotPasswordScreen,
-  LoginScreen,
-  ResetPasswordScreen,
-} from '@screens';
-import { ROUTE } from '@enums';
->>>>>>> 879dbcff550c53566b0fc529324b131c0f7ee84a
 
 const Stack = createNativeStackNavigator();
 
@@ -20,28 +10,8 @@ export const RootNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-<<<<<<< HEAD
           name="Notification"
           component={NotificationScreen}
-=======
-          name={ROUTE.LOGIN_SCREEN}
-          component={LoginScreen}
->>>>>>> 879dbcff550c53566b0fc529324b131c0f7ee84a
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={ROUTE.FORGOT_PASSWORD}
-          component={ForgotPasswordScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={ROUTE.FORGOT_MDP}
-          component={ForgotMdpScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name={ROUTE.RESET_PASSWORD}
-          component={ResetPasswordScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
