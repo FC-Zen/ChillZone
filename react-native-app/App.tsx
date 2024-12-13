cfdf9dd02e6494e3d8c4baf8fc97c67bbc1936eb
Thiiiv <thiiivpro@gmail.com>
2024-12-14 00:43:23 +0100

    CZEN- | fix: Ignore all the warning messages

--- react-native-app/App.tsx
+++ react-native-app/App.tsx
@@ -1,6 +1,8 @@
 import { RootNavigator } from '@navigation/RootNavigator';
 import '@translations/i18n';
+import { LogBox } from 'react-native';
 
 export default function App() {
+  LogBox.ignoreLogs(['Warning']); // Ignore les logs de type 'Warning'
   return <RootNavigator />;
 }
