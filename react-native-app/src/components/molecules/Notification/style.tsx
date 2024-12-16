a53d993976945d55011311e0ab83f5e97d34a30d
Thiiiv <thiiivpro@gmail.com>
2024-12-17 00:26:07 +0100

    CZEN-139 | fix: Fixes from main and Notifications displays fine

--- react-native-app/src/components/molecules/Notification/style.tsx
+++ react-native-app/src/components/molecules/Notification/style.tsx
@@ -15,6 +15,28 @@ export const styles = StyleSheet.create({
     backgroundColor: colors.white,
     gap: 10,
   },
+  outsideArea: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'transparent',
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContainer: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'hsla(0, 0%, 0%, 0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalContent: {
+    width: 'auto',
+    height: 'auto',
+    borderRadius: 10,
+  },
   iconContainer: {
     width: 50,
     height: 50,
