9839a88c5bdebd2fa67ba4e9748b6954f294be06
tahathierrysefoudine <tahathierry130@gmail.com>
2024-12-17 23:29:04 +0100

    CZEN-137 fix : bug d'affichage expo go

--- react-native-app/src/components/molecules/ActionButton/ActionButton.tsx
+++ react-native-app/src/components/molecules/ActionButton/ActionButton.tsx
@@ -14,7 +14,7 @@ export const ActionButton: React.FC<ActionButtonProps> = ({
   onPress,
 }) => (
   <Button
-    label={label}
+    title={label}
     onPress={onPress}
     // Forcer l'ajout d'une prop non déclarée
     {...({ icon: <Ionicons name={iconName} size={20} color="#fff" /> } as any)}
