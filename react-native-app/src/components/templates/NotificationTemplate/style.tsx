0d2f23832fdf34e3dc8d39ce18e73135b4a32f22
Thiiiv <thiiivpro@gmail.com>
2024-12-18 18:00:47 +0100

    CZEN-139 | fix: Scrollable notifications and display the time difference instead of notification's time

--- react-native-app/src/components/templates/NotificationTemplate/style.tsx
+++ react-native-app/src/components/templates/NotificationTemplate/style.tsx
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native';
 import { colors } from '@theme';
 
 export const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        width: '100%',
-        height: '80%',
-        gap: 20,
-    },
+  container: {
+    display: 'flex',
+    width: '100%',
+    height: '80%',
+    gap: 20,
+  },
 });
