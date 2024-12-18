485754a9b2a01f36ffde1a7b39c819ae7f7f7205
Thiiiv <thiiivpro@gmail.com>
2024-12-18 18:06:41 +0100

    CZEN-139 | fix: add padding and use of header height

--- react-native-app/src/components/molecules/PageHeader/PageHeader.tsx
+++ react-native-app/src/components/molecules/PageHeader/PageHeader.tsx
@@ -9,14 +9,25 @@ export type PageHeaderProps = {
   title: string;
   variant: 'default' | 'back';
   onBackPress?: () => void;
-}
+};
 
-export const PageHeader: React.FC<PageHeaderProps> = ({ title, variant, onBackPress }) => {
+export const PageHeader: React.FC<PageHeaderProps> = ({
+  title,
+  variant,
+  onBackPress,
+}) => {
   const headerHeight = useHeaderHeight();
   return (
     <View style={[styles.container, { top: headerHeight }]}>
       {variant == 'back' && (
-          <Icon name="Cross" color="#000" style={styles.icon} onPress={onBackPress} width={16} height={16} />
+        <Icon
+          name="Cross"
+          color="#000"
+          style={styles.icon}
+          onPress={onBackPress}
+          width={16}
+          height={16}
+        />
       )}
       <Text style={styles.title}>{title}</Text>
     </View>
--- react-native-app/src/components/molecules/PageHeader/style.tsx
+++ react-native-app/src/components/molecules/PageHeader/style.tsx
@@ -3,25 +3,25 @@ import { StyleSheet } from 'react-native';
 import { fonts } from '@theme/typography';
 
 export const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-        height: 'auto',
-        textAlign: 'center',
-        paddingVertical: '3%',
-        paddingHorizontal: '10%',
-        gap: '20%',
-    },
-    icon: {
-        width: 16,
-        height: 16,
-    },
-    title: {
-        color: colors.black, 
-        textAlign: 'center',
-        fontFamily: typography.h2.fontFamily,
-        fontSize: typography.h2.fontSize,
-    },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: 'auto',
+    textAlign: 'center',
+    paddingVertical: '3%',
+    paddingHorizontal: '10%',
+    gap: '20%',
+  },
+  icon: {
+    width: 16,
+    height: 16,
+  },
+  title: {
+    color: colors.black,
+    textAlign: 'center',
+    fontFamily: typography.h2.fontFamily,
+    fontSize: typography.h2.fontSize,
+  },
 });
