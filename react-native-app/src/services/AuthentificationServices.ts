09e7f7777bd4700340d8dcbe2e628dab6e77bcdd
Eliaslhl <eliaslahlouh@gmail.com>
2024-12-19 13:50:01 +0100

    CZEN-CONF | chore: add axios in aoo

--- react-native-app/src/services/AuthentificationServices.ts
+++ react-native-app/src/services/AuthentificationServices.ts
@@ -1,102 +1,139 @@
-import { Data } from '@assets';
+import axios from 'axios';
 import { z } from 'zod';
 
-const validEmail = Data.user_test.email; // Exemple d'email
-const validPassword = Data.user_test.password; // Exemple de mot de passe
+// Exemples d'authentification
+const validEmail = 'user@example.com';
+const validPassword = 'password123'; // Exemple de mot de passe
+const validEmails = ['user@example.com', 'admin@example.com'];
 
-// Service d'authentification
-export const authenticateUser = async (email: string, password: string) => {
-  const formData = {
-    email: email,
-    password: password,
-  };
-
-  // Vérification des informations d'authentification
-  if (formData.email !== validEmail || formData.password !== validPassword) {
-    throw new Error('Email ou mot de passe incorrect');
-  }
-
-  console.log('Authentification réussie pour:', formData.email);
-  return { success: true, message: 'Connexion réussie !' };
-};
-
-// Service de demande de réinitialisation de mot de passe
-export const requestPasswordReset = async (email: string) => {
-  const formData = { email: email };
-
-  if (formData.email !== validEmail) {
-    throw new Error('Email non trouvé');
-  }
-
-  // Simuler la génération d'un token unique
-  const resetToken = 'token-' + Math.random().toString(36).substr(2, 9);
-
-  // Simulation d'envoi d'email avec lien
-  const resetLink = `https://votreapp.com/reset-password?token=${resetToken}`;
-  console.log('Email envoyé à:', formData.email);
-  console.log('Lien de réinitialisation:', resetLink);
-
-  return {
-    success: true,
-    message: 'Un email de réinitialisation a été envoyé à votre adresse',
-  };
-};
-
-// Schéma Zod pour la réinitialisation du mot de passe
-const resetPasswordSchema = z
-  .object({
-    token: z.string().startsWith('token-', {
-      message: 'Lien de réinitialisation invalide ou expiré',
-    }),
-    newPassword: z
-      .string()
-      .min(12, {
-        message: 'Le mot de passe doit contenir au moins 12 caractères',
-      })
-      .max(16, {
-        message: 'Le mot de passe ne doit pas dépasser 16 caractères',
-      })
-      .regex(/[A-Z]/, {
-        message: 'Le mot de passe doit contenir au moins une lettre majuscule',
-      })
-      .regex(/[^a-zA-Z0-9]/, {
-        message: 'Le mot de passe doit contenir au moins un caractère spécial',
-      }),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.newPassword === data.confirmPassword, {
-    message: 'Les mots de passe ne correspondent pas',
-    path: ['confirmPassword'],
-  });
-
-export const resetPassword = async (
-  token: string,
-  newPassword: string,
-  confirmPassword: string
-) => {
+/**
+ * Authentifie un utilisateur en vérifiant ses informations de connexion.
+ *
+ * @param {Object} formData - Les données de connexion fournies par l'utilisateur. Contient le login et le password
+ * @throws {Error} Si les informations de connexion (email ou mot de passe) sont incorrectes.
+ *
+ * @returns {Promise<Object>} résultat de l'authentification.
+ */
+export const authenticateUser = async (formData: {
+  login: string;
+  password: string;
+}) => {
   try {
-    // Valider les données d'entrée avec Zod
-    const formData = resetPasswordSchema.parse({
-      token,
-      newPassword,
-      confirmPassword,
+    /* Envoi des données d'authentification à l'API
+    const response = await axios.post( URL , {
+      login: formData.login,
+      password: formData.password,
     });
-
-    // Si la validation passe, continuer le processus
-    console.log(
-      'Mot de passe réinitialisé avec succès pour le token:',
-      formData.token
-    );
-
-    return {
-      success: true,
-      message: 'Votre mot de passe a été réinitialisé avec succès',
-    };
-  } catch (error) {
-    // Gestion des erreurs Zod
-    if (error instanceof z.ZodError) {
-      throw new Error(error.errors.map((err) => err.message).join(', '));
+    */
+    // Vérifie si la réponse indique une réussite - SIMULATION
+    if (formData.login == validEmail && formData.password == validPassword) {
+      console.log('Authentification réussie pour:', formData.login);
+      return { success: true, message: 'Connexion réussie !' };
+    } else {
+      throw new Error('Email ou mot de passe incorrect');
     }
-    throw error;
+  } catch (error: any) {
+    console.error("Erreur lors de l'authentification:", error.message);
+    throw new Error(error.message);
+  }
+};
+
+/**
+ * Envoie un email pour la récupération du mot de passe.
+ *
+ * @param {string} formData: { email: string } - L'adresse email de l'utilisateur.
+ * @throws {Error} Si l'email n'existe pas dans la base de données.
+ *
+ * @returns {Promise<Object>} Résultat de l'envoi de l'email.
+ */
+export const sendPasswordRecoveryEmail = async (formData: {
+  email: string;
+}) => {
+  // Exemple d'une liste d'emails valides pour la simulation
+  try {
+    /* Envoi des données à l'API
+    const response = await axios.post( URL , {
+      email,
+    });
+    */
+    // Vérifie si l'email est valide - SIMULATION
+    if (validEmails.includes(formData.email)) {
+      console.log(`Email de récupération envoyé à : ${formData.email}`);
+      return {
+        success: true,
+        message: 'Email de récupération envoyé avec succès !',
+      };
+    } else {
+      throw new Error("L'email n'existe pas");
+    }
+  } catch (error: any) {
+    console.error(
+      "Erreur lors de l'envoi de l'email de récupération :",
+      formData.email
+    );
+    throw new Error(error.message);
+  }
+};
+
+// Schéma CNIL
+const passwordSchema = z
+  .string()
+  .min(12, 'Le mot de passe doit contenir au moins 12 caractères.')
+  .regex(
+    /[A-Z]/,
+    'Le mot de passe doit contenir au moins une lettre majuscule.'
+  )
+  .regex(
+    /[a-z]/,
+    'Le mot de passe doit contenir au moins une lettre minuscule.'
+  )
+  .regex(/[0-9]/, 'Le mot de passe doit contenir au moins un chiffre.')
+  .regex(
+    /[!@#$%^&*(),.?":{}|<>]/,
+    'Le mot de passe doit contenir au moins un caractère spécial.'
+  );
+
+/**
+ * Réinitialise le mdp du compte
+ *
+ * @param {string} formData: { email: string } - L'adresse email de l'utilisateur.
+ * @throws {Error} Si l'email n'existe pas dans la base de données.
+ *
+ * @returns {Promise<Object>} Résultat de l'envoi de l'email.
+ */
+export const changePassword = async (formData: {
+  email: string;
+  inputPassword: string;
+  inputVerifyPassword: string;
+}) => {
+  try {
+    // Vérification de la correspondance des mots de passe
+    if (formData.inputPassword !== formData.inputVerifyPassword) {
+      throw new Error(
+        'Le nouveau mot de passe et la confirmation ne correspondent pas.'
+      );
+    }
+
+    // Validation des mots de passe via Zod
+    passwordSchema.parse(formData.inputPassword);
+
+    // Simule l'envoi de la demande de changement de mot de passe - ATTENTION A VOIR AVEC LE BACK LA DEMARCHE
+    /*
+    const response = await axios.post(URL, {
+      newPassword: formData.inputPassword,
+    });
+    */
+
+    console.log('Mot de passe changé avec succès pour l’utilisateur.');
+
+    // Simule une réponse réussie
+    return { success: true, message: 'Mot de passe changé avec succès !' };
+  } catch (error: any) {
+    if (error instanceof z.ZodError) {
+      console.error('Erreur de validation :', error.errors);
+      throw new Error(error.errors.map((e) => e.message).join(' '));
+    }
+    console.error('Erreur lors du changement de mot de passe:', error.message);
+    throw new Error(error.message);
   }
 };
