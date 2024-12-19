c497976ae9eae662a4a709d90e1b6d629ef2103a
Thiiiv <thiiivpro@gmail.com>
2024-12-19 15:02:46 +0100

    CZEN-139 | fix: Transfer the diff_time function from Notification Screen to Notification Item

--- react-native-app/src/components/molecules/Notification/NotificationItem.tsx
+++ react-native-app/src/components/molecules/Notification/NotificationItem.tsx
@@ -4,6 +4,52 @@ import { View, Text, Modal, TouchableOpacity } from 'react-native';
 import { Icon } from '@components/atoms';
 import { styles } from './style';
 import { NotificationPopup } from '@components/organisms/NotificationPopup/NotificationPopup';
+import { useTranslation } from 'react-i18next';
+
+function diff_time(date1: Date, date2: Date) {
+  const { t } = useTranslation();
+
+  // Calculer la différence de temps entre les deux dates
+  let timeDiff = date2.getTime() - date1.getTime();
+
+  // Retourner la différence de temps en fonction de l'unité de temps
+  // 1000 = 1 seconde
+  if (timeDiff < 1000) {
+    return t('timeUnit.now');
+  }
+  
+  else if (timeDiff < 1000 * 60) {
+    // Si la différence de temps est inférieure à 2 minutes, retourner "1 minute" au lieu de "1 minutes"
+    if (timeDiff < 1000 * 2) {
+      return Math.floor(timeDiff / 1000) + ' ' + t('timeUnit.second');
+    }
+    return Math.floor(timeDiff / 1000) + ' ' + t('timeUnit.second') + 's';
+  } 
+  
+  else if (timeDiff < 1000 * 60 * 60) {
+    // La même logique s'applique pour les minutes
+    if (timeDiff < 1000 * 60 * 2) {
+      return Math.floor(timeDiff / 60000) + ' ' + t('timeUnit.minute');
+    }
+    return Math.floor(timeDiff / 60000) + ' ' + t('timeUnit.minute') + 's';
+  } 
+  
+  else if (timeDiff < 86400000) {
+    // Pour les heures
+    if (timeDiff < 7200000) {
+      return Math.floor(timeDiff / 3600000) + ' ' + t('timeUnit.hour');
+    }
+    return Math.floor(timeDiff / 3600000) + ' ' + t('timeUnit.hour') + 's';
+  } 
+  
+  else {
+    // Et pour les jours
+    if (timeDiff < 172800000) {
+      return Math.floor(timeDiff / 86400000) + ' ' + t('timeUnit.day');
+    }
+    return Math.floor(timeDiff / 86400000) + ' ' + t('timeUnit.day') + 's';
+  }
+}
 
 export type NotificationItemProps = {
   title: string;
@@ -35,7 +81,7 @@ export const NotificationItem: React.FC<NotificationItemProps> = ({
           <View style={styles.modalContainer} >
           <TouchableOpacity style={styles.outsideArea} onPress={() => {setModalVisible(false);}} activeOpacity={1}>  
             <TouchableOpacity style={styles.modalContent} activeOpacity={1}>  
-              <NotificationPopup title={title} description={description} date={time} handlePress={() => setModalVisible(false)}></NotificationPopup>
+              <NotificationPopup title={title} description={description} date={new Date(time).toDateString()} handlePress={() => setModalVisible(false)}></NotificationPopup>
             </TouchableOpacity>
           </TouchableOpacity>
           </View>
@@ -51,7 +97,7 @@ export const NotificationItem: React.FC<NotificationItemProps> = ({
             {description}
           </Text>
         </View>
-        <Text style={styles.time}>{time}</Text>
+        <Text style={styles.time}>{diff_time(new Date(time), new Date())}</Text>
       </TouchableOpacity >
       
     </View>
