b9221a148f2936792db30f8174ca685942ca1ed9
Héléna Chevalier <lna.chevalier@gmail.com>
2024-12-23 23:11:10 +0100

    CZEN-136 | refactor : util mapping images

---
+++ react-native-app/src/utils/ImagesMap.tsx
@@ -0,0 +1,6 @@
+export const ImagesMap: { [key: string]: any } = {
+    'restaurant_image.png': require('@assets/data/Images_test/restaurant_image.png'),
+    'meal.png': require('@assets/data/Images_test/meal.png'),
+    'plan.png': require('@assets/data/Images_test/plan.png'),
+    'qrcode.png': require('@assets/data/Images_test/qrcode.png'),
+};
