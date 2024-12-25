63839ef4942d24afb71d503a9c9ece04945c8127
LoicMauritius <loic.mauritius@gmail.com>
2024-12-25 23:56:36 +0100

    CZEN-138 - Add props to the Accordion to make it neutral

--- react-native-app/src/components/molecules/Accordion/Accordion.tsx
+++ react-native-app/src/components/molecules/Accordion/Accordion.tsx
@@ -1,17 +1,20 @@
-import { FaqDetail } from '@services/FaqServices';
 import React from 'react';
 import { List } from 'react-native-paper';
 import { Text } from 'react-native';
 import { styles } from './style';
 import { Icon } from '@components/atoms';
-import { colors } from '@theme';
+
+type AccordionProps = {
+  question: string;
+  answer: string;
+}; //Dois toujours être égale à FaqDetail de '@services/FaqServices' pour l'intégration de la Faq 
 
 /**
  * @param detail instance of FaqDetail
  *  
  * @returns An accordion with the question as a title and the answer below when you click on it
  */
-export const Accordion: React.FC<FaqDetail> = (detail:FaqDetail) => {
+export const Accordion: React.FC<AccordionProps> = (detail) => {
   return (
     <>
       <List.Accordion 
