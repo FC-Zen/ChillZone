c043fe57f49730151a0102035cf71163e8a835dd
Thiiiv <thiiivpro@gmail.com>
2024-12-27 00:37:54 +0100

    CZEN-155 | feat: New Input variant (Input with subtitle)

--- react-native-app/src/components/molecules/Input/Input.tsx
+++ react-native-app/src/components/molecules/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react';
-import { StyleProp, TextInput, ViewStyle } from 'react-native';
+import { StyleProp, TextInput, View, ViewStyle, Text } from 'react-native';
 import { InputWrapper, InputIcon, InputStyles } from './style';
 import { Icon, IconProps } from '@components/atoms';
 import { colors } from '@theme';
@@ -9,8 +9,10 @@ export type InputProps = {
   icon?: IconProps['name'];
   onChangeText: (text: string) => void;
   value: string;
-  variant?: 'default' | 'password';
+  variant?: 'default' | 'password' | 'subtitled';
   style?: StyleProp<ViewStyle>;
+  subtitle?: string;
+  subtitleColor?: string;
 };
 
 export const Input: FC<InputProps> = ({
@@ -20,33 +22,38 @@ export const Input: FC<InputProps> = ({
   value,
   variant = 'default',
   style,
+  subtitle = '',
+  subtitleColor,
 }) => {
   const [isPasswordVisible, setPasswordVisible] = useState(
     variant === 'password'
   );
 
   return (
-    <InputWrapper
-      style={style}
-      placeholder={placeholder}
-      onChangeText={onChangeText}
-      value={value}
-    >
-      {icon && (
-        <InputIcon>
-          <Icon name={icon} />
-        </InputIcon>
-      )}{' '}
-      {/* Passer l'icône ici */}
-      <TextInput
-        style={InputStyles.input}
+    <View style={variant === 'subtitled' && subtitle !== '' ? InputStyles.subtitledContainer : {}}>
+      <InputWrapper
+        style={style}
         placeholder={placeholder}
         onChangeText={onChangeText}
         value={value}
-        secureTextEntry={isPasswordVisible}
-        placeholderTextColor={colors.silver}
-      />
-    </InputWrapper>
+      >
+        {icon && (
+          <InputIcon>
+            <Icon name={icon} />
+          </InputIcon>
+        )}{' '}
+        {/* Passer l'icône ici */}
+        <TextInput
+          style={InputStyles.input}
+          placeholder={placeholder}
+          onChangeText={onChangeText}
+          value={value}
+          secureTextEntry={isPasswordVisible}
+          placeholderTextColor={colors.silver}
+        />
+      </InputWrapper>
+      {variant === 'subtitled' && subtitle !== '' && <Text style={{color: subtitleColor}} >{subtitle}</Text>}
+    </View>
   );
 };
 
