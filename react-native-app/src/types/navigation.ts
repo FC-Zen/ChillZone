b867e29d835474ba521f148aa851ddbfb33d6cf9
tahathierrysefoudine <tahathierry130@gmail.com>
2024-12-26 22:40:56 +0100

    CZEN-137 | feat : add PageHeader into the templte and styling AccountScrenn

--- react-native-app/src/types/navigation.ts
+++ react-native-app/src/types/navigation.ts
@@ -1,6 +1,7 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
+  AccountScrenn: undefined;
   Profile: { userId: string };
   Settings: undefined;
 };
