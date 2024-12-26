9c18da382df589b774fd306b8dd8f7ed1875b753
Eliaslhl <eliaslahlouh@gmail.com>
2024-12-26 23:01:11 +0100

    CZEN-152 | feat: add FoodCardList component

---
+++ react-native-app/src/components/organisms/FoodCardList/index.ts
@@ -0,0 +1,1 @@
+export { FoodCardList } from './FoodCardList';
---
+++ react-native-app/src/components/organisms/FoodCardList/style.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  contentContainer: {
+    flexDirection: 'column',
+    padding: 16,
+    gap: 16,
+  },
+  cardContainer: {
+    marginBottom: 12,
+  },
+});
