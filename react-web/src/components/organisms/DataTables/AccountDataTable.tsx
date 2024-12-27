259416aaceca1a4d32cbbf129c4ee184c469c916
Eliaslhl <eliaslahlouh@gmail.com>
2024-12-27 15:25:46 +0100

    CZEN-147 | refactor: refact AccountDataTable

--- react-web/src/components/organisms/DataTables/AccountDataTable.tsx
+++ react-web/src/components/organisms/DataTables/AccountDataTable.tsx
@@ -1,5 +1,6 @@
 import {
   DataGrid,
+  GridColDef,
   GridToolbarQuickFilter,
   GridToolbarContainer,
   GridToolbarColumnsButton,
@@ -7,10 +8,9 @@ import {
   GridToolbarExport,
   GridToolbarDensitySelector,
 } from '@mui/x-data-grid';
-import { Button, IconButton, Switch } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 import './style.css';
-import { DeleteAccount, Icon } from '@components/atoms/Icons';
-import { EditIcon } from 'lucide-react';
+import { Icon } from '@components/atoms/Icons';
 import { CustomSwitch } from '@components/molecules';
 
 type AccountDataTableProps = {
@@ -21,7 +21,7 @@ type AccountDataTableProps = {
     id: number;
     first_name: string;
     last_name: string;
-    email : string;
+    email: string;
     role: string;
     establishment: string;
     reservation_count: number;
@@ -39,10 +39,14 @@ function CustomToolbar({ onActionClick }: { onActionClick: () => void }) {
 
       <GridToolbarColumnsButton />
       <GridToolbarFilterButton />
-      <GridToolbarDensitySelector/>
-      <GridToolbarExport/>
+      <GridToolbarDensitySelector />
+      <GridToolbarExport />
 
-      <Button variant="contained" className="customBtnDataTable" onClick={onActionClick} >
+      <Button
+        variant="contained"
+        className="customBtnDataTable"
+        onClick={onActionClick}
+      >
         <Icon name="AddIcon" />
         Ajouter une salle
       </Button>
@@ -50,51 +54,57 @@ function CustomToolbar({ onActionClick }: { onActionClick: () => void }) {
   );
 }
 
-export const AccountDataTable = ({ 
+export const AccountDataTable = ({
   addAccountBtn,
   deleteBtn,
-  toggleBtn, 
-  data 
-  }: AccountDataTableProps) => {
-
-    const columns = [
-      { field: 'id', headerName: 'ID', flex: 0.5 },
-      { field: 'first_name', headerName: 'Prénom', flex: 2 },
-      { field: 'last_name', headerName: 'Nom', flex: 2 },
-      { field: 'role', headerName: 'Rôle', flex: 1.5 },
-      { field: 'establishment', headerName: 'Établissement', flex: 3 },
-      { field: 'reservation_count', headerName: 'Nombre de réservations faites', flex: 2 },
-      {
-        field: 'toggle', headerName: 'Bloquer les réservations', flex: 2, align: 'center', 
-        renderCell: (params: any) => {
-          const isActive = params.row.status === 'Blocked';
-          return (
-            <CustomSwitch
-              checked={isActive}
-              onChange={() => {
-                const newStatus = isActive ? 'Verified' : 'Blocked'; 
-                console.log(isActive);
-                console.log(newStatus);
-                toggleBtn(params.row.id, newStatus);
-              }}
-            />
-          );
-        },
+  toggleBtn,
+  data,
+}: AccountDataTableProps) => {
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', flex: 0.5 },
+    { field: 'first_name', headerName: 'Prénom', flex: 2 },
+    { field: 'last_name', headerName: 'Nom', flex: 2 },
+    { field: 'role', headerName: 'Rôle', flex: 1.5 },
+    { field: 'establishment', headerName: 'Établissement', flex: 3 },
+    {
+      field: 'reservation_count',
+      headerName: 'Nombre de réservations faites',
+      flex: 2,
+    },
+    {
+      field: 'toggle',
+      headerName: 'Bloquer les réservations',
+      flex: 2,
+      align: 'center',
+      renderCell: (params: any) => {
+        const isActive = params.row.status === 'Blocked';
+        return (
+          <CustomSwitch
+            checked={isActive}
+            onChange={() => {
+              const newStatus = isActive ? 'Verified' : 'Blocked';
+              console.log(isActive);
+              console.log(newStatus);
+              toggleBtn(params.row.id, newStatus);
+            }}
+          />
+        );
       },
-      {
-        field: 'actions', headerName: 'Supprimer le compte', flex: 2, sortable: false,
-        renderCell: (params: any) => (
-          <div className="flex gap-2 justify-center">
-            <IconButton
-              color="primary"
-              onClick={() => deleteBtn(params.row.id)}
-            >
-              <Icon name="DeleteAccount" />
-            </IconButton>
-          </div>
-        ),
-      },
-    ];
+    },
+    {
+      field: 'actions',
+      headerName: 'Supprimer le compte',
+      flex: 2,
+      sortable: false,
+      renderCell: (params: any) => (
+        <div className="flex gap-2 justify-center">
+          <IconButton color="primary" onClick={() => deleteBtn(params.row.id)}>
+            <Icon name="DeleteAccount" />
+          </IconButton>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <div style={{ height: 700, width: '100%' }}>
