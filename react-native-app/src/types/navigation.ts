9ad385b2537383c796d065fa15756e652317f7ad
Eliaslhl <eliaslahlouh@gmail.com>
2024-12-30 20:02:25 +0100

    CZEN-137 | fix: fix problems screen

--- react-native-app/src/types/navigation.ts
+++ react-native-app/src/types/navigation.ts
@@ -1,7 +1,6 @@
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export type RootStackParamList = {
-  AccountScrenn: undefined;
   Profile: { userId: string };
   Settings: undefined;
 };
