20902d80847ab7b4983802cf597c2bc4a5f76fed
Thiiiv <thiiivpro@gmail.com>
2025-01-06 21:06:32 +0100

    CZEN-156 | feat: Add some code to handle commands

--- react-native-app/src/contexts/AppContrext/AppContext.tsx
+++ react-native-app/src/contexts/AppContrext/AppContext.tsx
@@ -26,3 +26,37 @@ export const useUser = () => {
   }
   return context;
 };
+
+export type CommandContextType = {
+  commandId: string | null;
+  listItems: string[];
+  totalAmount: number;
+  setCommandId: (id: string) => void;
+  setListItems: (items: string[]) => void;
+  setTotalAmount: (amount: number) => void;
+};
+
+export const CommandContext = createContext<CommandContextType | undefined>(undefined);
+
+export const CommandProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [commandId, setCommandId] = useState<string | null>(null);
+  const [listItems, setListItems] = useState<string[]>([]);
+  const [totalAmount, setTotalAmount] = useState<number>(0);
+
+  return (
+    <CommandContext.Provider value={{ commandId, listItems, totalAmount, setCommandId, setListItems, setTotalAmount }}>
+      {children}
+    </CommandContext.Provider>
+  );
+}
+
+export const useCommand = () => {
+  const context = useContext(CommandContext);
+  if (!context) {
+    throw new Error('useCommand must be used within a CommandProvider');
+  }
+  return context;
+};
+
