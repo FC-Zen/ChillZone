56f26855685b4e2ee1fcd876fb859b83a8012cf1
Eliaslhl <eliaslahlouh@gmail.com>
2025-01-06 15:51:29 +0100

    CZEN-192 | feat: add reservation template

--- react-native-app/src/screens/ReservationScreen/ReservationScreen.tsx
+++ react-native-app/src/screens/ReservationScreen/ReservationScreen.tsx
@@ -97,25 +97,6 @@ export const ReservationScreen = () => {
           onPress: () => console.log('Reserve button pressed'),
         }}
       />
-      <Input
-        placeholder="Search"
-        icon="Cube"
-        subIcon="Lock"
-        variant="select"
-        data={['Option 1', 'Option 2']}
-        onSelect={(selected) => console.log('Selected:', selected)}
-        style={{ marginTop: 20 }}
-      />
-      <Input
-        placeholder="Search"
-        icon="Cube"
-        subIcon="Lock"
-        variant="select"
-        data={['Option 1', 'Option 2']}
-        onSelect={(selected) => console.log('Selected:', selected)}
-        style={{ marginTop: 20 }}
-      />
-
       <BottomNavbar activeIcon="Reserve" />
     </View>
   );
