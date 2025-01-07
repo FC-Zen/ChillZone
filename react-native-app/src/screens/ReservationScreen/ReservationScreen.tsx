0b5e6057281e6a517dc436ee16bcc4f436b106c3
Eliaslhl <eliaslahlouh@gmail.com>
2025-01-07 01:25:30 +0100

    CZEN-192 | feat: add reservation services

--- react-native-app/src/screens/ReservationScreen/ReservationScreen.tsx
+++ react-native-app/src/screens/ReservationScreen/ReservationScreen.tsx
@@ -1,37 +1,77 @@
+import { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { ReservationTemplate } from '@components/templates';
 import { useTranslation } from 'react-i18next';
 import { ReservationTemplateProps } from '@components/templates/ReservationTemplate/ReservationTemplate';
 import { BottomNavbar, TopBar } from '@components';
 import { styles } from './style';
-import { colors } from '@theme';
+import { getReservations } from '@services';
 
 export const ReservationScreen = () => {
   const { t } = useTranslation();
+  const [dayReservations, setDayReservations] = useState<string[]>([]);
+  const [timeSlots, setTimeSlots] = useState<string[]>([]);
+  const [durations, setDurations] = useState<string[]>([]);
+
+  const calculateDuration = (startTime: string, endTime: string): string => {
+    const [startHours, startMinutes] = startTime.split('h').map(Number);
+    const [endHours, endMinutes] = endTime.split('h').map(Number);
+
+    const startTotalMinutes = startHours * 60 + (startMinutes || 0);
+    const endTotalMinutes = endHours * 60 + (endMinutes || 0);
+
+    const durationHours = Math.abs(endTotalMinutes - startTotalMinutes) / 60;
+
+    return durationHours === 1 ? 'Court : 1h' : 'Long : 2h';
+  };
+
+  useEffect(() => {
+    const fetchReservations = async () => {
+      const reservations = await getReservations();
+
+      const dates = reservations.map(
+        (reservation) => reservation.day_reservation
+      );
+      setDayReservations(dates);
+
+      const times = reservations.map(
+        (reservation) => `${reservation.start_time} - ${reservation.end_time}`
+      );
+      setTimeSlots(times);
+
+      const calculatedDurations = reservations.map((reservation) =>
+        calculateDuration(reservation.start_time, reservation.end_time)
+      );
+      setDurations(calculatedDurations);
+    };
+
+    fetchReservations();
+  }, []);
 
   const inputs: ReservationTemplateProps['inputs'] = [
     [
       {
         placeholder: t('fields.room.type'),
         icon: 'Expand',
-        subIcon: 'Lock',
+        subIcon: 'School',
         variant: 'select',
-        data: ['Option 1', 'Option 2'],
+        data: [t('filters.acoustic'), t('filters.classroom')],
         onSelect: (selected: string) => console.log('Selected:', selected),
       },
       {
         placeholder: t('fields.common.date'),
         icon: 'Calendar',
         variant: 'select',
-        data: ['Option 1', 'Option 2'],
-        onSelect: (selected: string) => console.log('Selected:', selected),
+        data: dayReservations,
+        onSelect: (selected: string) => console.log('Selected date:', selected),
       },
       {
         placeholder: t('fields.room.hours'),
         icon: 'Clock',
         variant: 'select',
-        data: ['Option 1', 'Option 2'],
-        onSelect: (selected: string) => console.log('Selected:', selected),
+        data: durations,
+        onSelect: (selected: string) =>
+          console.log('Selected time slot:', selected),
       },
     ],
     [
@@ -39,14 +79,14 @@ export const ReservationScreen = () => {
         placeholder: t('fields.room.schedules'),
         icon: 'Calendar',
         variant: 'select',
-        data: ['Option 1', 'Option 2'],
+        data: timeSlots,
         onSelect: (selected: string) => console.log('Selected:', selected),
       },
     ],
   ];
 
   const roomSelectorProps = {
-    title: 'Salles disponibles',
+    title: t('filters.roomsOpen'),
     rooms: [
       {
         label: 'N°1',
@@ -90,7 +130,7 @@ export const ReservationScreen = () => {
         inputs={inputs}
         titleHeader={t('headers.reservation')}
         subTitle={t('filters.filterTitle')}
-        subTitle2={'Horaires disponibles'}
+        subTitle2={t('filters.hoursOpen')}
         roomSelectorProps={roomSelectorProps}
         buttonProps={{
           title: t('headers.reservation'),
---
+++ react-native-app/src/services/RoomServices.ts
@@ -0,0 +1,37 @@
+import { rooms } from '@assets/data/rooms.json';
+
+export type Room = {
+  id: number;
+  name: string;
+  description: string;
+  capacity: number;
+  status: boolean;
+  position_x: number;
+  position_y: number;
+  floor: number;
+  photo_link: string;
+  tag_label: string;
+};
+
+export type RoomsResponse = {
+  rooms: Room[];
+};
+
+// Fonction pour récupérer toutes les salles
+export const getRooms = async (): Promise<Room[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(rooms);
+    }, 1000);
+  });
+};
+
+// Fonction pour récupérer une salle par son ID
+export const getRoomById = async (id: number): Promise<Room | undefined> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const room = rooms.find((room: Room) => room.id === id);
+      resolve(room);
+    }, 1000);
+  });
+};
