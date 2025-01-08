50744800fd8916016385c4b34a568527bd4bb8c5
Héléna Chevalier <lna.chevalier@gmail.com>
2025-01-08 21:58:41 +0100

    CZEN-149 | feat : AutoCompleteInput

--- react-web/src/components/molecules/index.ts
+++ react-web/src/components/molecules/index.ts
@@ -1,7 +1,10 @@
 export { Input } from './Input';
+export { FileInput } from './FileInput';
+export { AutoCompleteInput } from './AutoCompleteInput';
+
 export { VectorHeader, VectorBottom } from './Vectors';
 export { SnackBar } from './SnackBar';
 export { CustomSwitch } from './Switch';
-export {StatCard} from './StatCard';
+export { StatCard } from './StatCard';
 export { NavigationButton } from './NavigationButton';
 export { ModalHeader } from './ModalHeader';
