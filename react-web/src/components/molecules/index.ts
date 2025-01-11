810d2c8ca245acb4213948a0ff6498de775b6f08
LoicMauritius <loic.mauritius@gmail.com>
2025-01-11 20:01:14 +0100

    CZEN-143 - Create some molecules, one Select for the Select with only one possibility and CommandInfo for the command Modal

--- react-web/src/components/molecules/index.ts
+++ react-web/src/components/molecules/index.ts
@@ -1,13 +1,12 @@
 export { Input } from './Input';
 export { FileInput } from './FileInput';
 export { AutoCompleteInput } from './AutoCompleteInput';
-
 export { VectorHeader, VectorBottom } from './Vectors';
 export { SnackBar } from './SnackBar';
 export { CustomSwitch } from './Switch';
 export { StatCard } from './StatCard';
 export { NavigationButton } from './NavigationButton';
 export { ModalHeader } from './ModalHeader';
-
 export { Map } from './Map';
-
+export { StyledSelect } from './Select';
+export { CommandInfo } from './CommandInfo';
