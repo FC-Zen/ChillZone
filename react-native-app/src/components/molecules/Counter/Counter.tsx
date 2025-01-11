c004f42597a2b2359a7c6462baee93ee94a63d83
Thiiiv <thiiivpro@gmail.com>
2025-01-11 20:22:11 +0100

    CZEN-154 | feat: Add new variant small for cart screen

--- react-native-app/src/components/molecules/Counter/Counter.tsx
+++ react-native-app/src/components/molecules/Counter/Counter.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleProp, ViewStyle } from 'react-native';
 import { Icon } from '@components/atoms';
 import { styles } from './style';
+import { typography } from '@theme';
 
 type CounterProps = {
   quantity: number;
   onIncrement: () => void;
   onDecrement: () => void;
+  onDelete?: () => void;
+  variant?: 'default' | 'small';
 };
 
 export const Counter: React.FC<CounterProps> = ({
   quantity,
   onIncrement,
   onDecrement,
+  onDelete,
+  variant = 'default',
 }) => {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, variant === 'small' && { width: 80, paddingHorizontal: 0, height: 28 }]}>
       <TouchableOpacity style={styles.button}>
-        <Icon name="Less" onPress={onDecrement} />
+        { variant === 'small' && quantity <= 1 ? 
+        <Icon name="Trash" onPress={onDelete} /> 
+        : <Icon name="Less" onPress={onDecrement} />}
       </TouchableOpacity>
-      <Text style={styles.quantity}>{quantity}</Text>
+      <Text style={[styles.quantity, variant === 'small' && { fontSize: 13, fontFamily: typography.h0.fontFamily }]}>{quantity}</Text>
       <TouchableOpacity style={styles.button}>
         <Icon name="More" onPress={onIncrement} />
       </TouchableOpacity>
