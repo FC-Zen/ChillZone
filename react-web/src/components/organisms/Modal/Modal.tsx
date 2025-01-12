514a594ae507e6fb27a0ef88a40a28f3dc7cefc7
LoicMauritius <loic.mauritius@gmail.com>
2025-01-12 12:10:44 +0100

    CZEN-143 - Add a component Modal

---
+++ react-web/src/components/organisms/Modal/Modal.tsx
@@ -0,0 +1,35 @@
+import { InputField } from '@components/organisms/ModalForm/ModalForm';
+import { styles } from './style';
+import { ModalHeader } from '@components/molecules';
+import { ModalForm } from '@components/organisms';
+
+type Modalprops = {
+  isOpen: boolean;
+  onClose: () => void;
+  handleForm: (formData: FormData) => void;
+  title: string;
+  listInputs: InputField[];
+};
+
+export const Modal = ({
+  isOpen,
+  onClose,
+  handleForm,
+  title,
+  listInputs,
+}: Modalprops) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50" >
+        <div className="bg-white rounded-lg shadow-lg p-6 w-1/3">
+          <ModalHeader title={title} onClose={onClose} />
+        <div style={styles.body} >
+          <ModalForm onSubmit={handleForm} listInputs={listInputs} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
