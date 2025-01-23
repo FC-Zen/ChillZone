c200da13bab1398bec045ac1c05c60ead0e479de
Héléna Chevalier <lna.chevalier@gmail.com>
2025-01-23 15:21:05 +0100

    CZEN-229 | feat : protected routes

--- react-web/src/App.tsx
+++ react-web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import {
   LoginPage,
   ForgotMdpPage,
@@ -17,33 +17,45 @@ import {
   SuperAdminPage
 } from '@pages';
 import { UserProvider } from '@hooks/UserContext/UserContext';
+import { ProtectedRoute } from '@navigation';
 
 function App() {
+
   return (
     <UserProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/forgot-mdp" element={<ForgotMdpPage />} />
-        <Route path="/sign-up" element={<SignUpOwnerPage />} />
+      <Router>
+        <Routes>
+          {/* Routes publiques */}
+          <Route element={<ProtectedRoute allowedRoles={['None']} />}>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/forgot-mdp" element={<ForgotMdpPage />} />
+            <Route path="/sign-up" element={<SignUpOwnerPage />} />
+          </Route>
 
-        <Route path="/admin-dashboard" element={<AdminHomePage />} />
-        <Route path="/admin-accounts" element={<AdminAccountPage />} />
-        <Route path="/admin-rooms" element={<AdminRoomsPage />} />
-        <Route path="/admin-booking" element={<AdminBookingPage />} />
-        <Route path="/admin-establishment" element={<AdminEstablishmentPage />} />
-        <Route path="/admin-affiliates" element={<AdminOwnerPage />} />
+          {/* Routes protégées par rôles */}
+          <Route element={<ProtectedRoute allowedRoles={['Administrateur', 'Super-Administrateur']} />}>
+            <Route path="/admin-dashboard" element={<AdminHomePage />} />
+            <Route path="/admin-accounts" element={<AdminAccountPage />} />
+            <Route path="/admin-rooms" element={<AdminRoomsPage />} />
+            <Route path="/admin-booking" element={<AdminBookingPage />} />
+            <Route path="/admin-establishment" element={<AdminEstablishmentPage />} />
+            <Route path="/admin-affiliates" element={<AdminOwnerPage />} />
+          </Route>
 
-        <Route path="/owner-dashboard" element={<OwnerHomePage />} />
-        <Route path="/owner-meals" element={< OwnerMealsPage />} />
-        <Route path="/owner-orders" element={<OwnerOrdersPage />} />
+          <Route element={<ProtectedRoute allowedRoles={['Restaurateur', 'Super-Administrateur']} />}>
+            <Route path="/owner-dashboard" element={<OwnerHomePage />} />
+            <Route path="/owner-meals" element={<OwnerMealsPage />} />
+            <Route path="/owner-orders" element={<OwnerOrdersPage />} />
+          </Route>
 
-        <Route path="/super-admin" element={<SuperAdminPage />} />
-      </Routes>
-    </Router>
+          <Route element={<ProtectedRoute allowedRoles={['Super-Administrateur']} />}>
+            <Route path="/super-admin" element={<SuperAdminPage />} />
+          </Route>
+        </Routes>
+      </Router>
     </UserProvider>
   );
 }
--- react-web/src/navigation/ProtectedRoute/index.ts
+++ react-web/src/navigation/ProtectedRoute/index.ts
@@ -1,2 +1,1 @@
 export { ProtectedRoute } from './ProtectedRoute';
-export { RootNavigator } from './RootNavigator';
