2a54cb1a90850dfc51faa90648b684079dd8f55a
tahathierrysefoudine <tahathierry130@gmail.com>
2025-01-31 17:31:19 +0100

    Revert " CZEN-197| fix : corrections reviews"
    This reverts commit ce5a105cd0ff02cfbdda7c1a05123046a2ebbdce.

--- react-native-app/src/components/atoms/index.ts
+++ react-native-app/src/components/atoms/index.ts
@@ -1,3 +1,5 @@
 export { Icon, IconProps } from './Icons';
 export { Checkbox, CheckboxProps } from './Checkbox';
 export { Text, TextProps } from './Text';
+export { Separation } from './Separation';
+
