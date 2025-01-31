375b8bcfb37ccb070a54ab52c8cdf9d257e59bf8
tahathierrysefoudine <tahathierry130@gmail.com>
2025-01-31 16:38:23 +0100

    Revert " CZEN-197| fix : corrections reviews"
    This reverts commit ce5a105cd0ff02cfbdda7c1a05123046a2ebbdce.

--- react-native-app/src/components/organisms/AdeModal/AdeModal.tsx
+++ react-native-app/src/components/organisms/AdeModal/AdeModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Modal, TouchableOpacity } from 'react-native';
 import { Input } from '@components/molecules';
 import { Icon } from '@components/atoms/Icons';
@@ -6,11 +6,11 @@ import { colors } from '@theme';
 import { styles } from './style';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@components/molecules';
+import { HelpModal } from '@components/organisms'; // Importez la modale d'aide
 
 export type AdeModalProps = {
   isOpen: boolean;
   onClose: () => void;
-  onOpenHelp: () => void; // Géré par le parent
   adeLink: string;
   setAdeLink: (link: string) => void;
   onSubmit: () => void;
@@ -19,12 +19,12 @@ export type AdeModalProps = {
 export const AdeModal = ({
   isOpen,
   onClose,
-  onOpenHelp,
   adeLink,
   setAdeLink,
   onSubmit,
 }: AdeModalProps) => {
   const { t } = useTranslation();
+  const [isHelpModalOpen, setHelpModalOpen] = useState(false); // État pour ouvrir/fermer la modale d'aide
 
   return (
     <Modal
@@ -42,7 +42,7 @@ export const AdeModal = ({
               variant="icon"
               style={styles.helpButton}
               textColor={colors.aquaDeep}
-              onPress={onOpenHelp} // Passé par le parent
+              onPress={() => setHelpModalOpen(true)} // Ouvre la modale d'aide
             />
           </View>
 
@@ -70,9 +70,21 @@ export const AdeModal = ({
           />
 
           {/* Bouton Fermer */}
-          <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
-            <Icon name="Cross" width={15} height={15} color={colors.white} />
+          <TouchableOpacity style={styles.closeIcon}>
+            <Icon
+              name="Cross"
+              width={15}
+              height={15}
+              color={colors.white}
+              onPress={onClose}
+            />
           </TouchableOpacity>
+
+          {/* Modale d'Aide */}
+          <HelpModal
+            isOpen={isHelpModalOpen}
+            onClose={() => setHelpModalOpen(false)} // Ferme la modale d'aide
+          />
         </View>
       </View>
     </Modal>
