49a3e04895b270ca87629d71e2d1965d293036c8
tahathierrysefoudine <tahathierry130@gmail.com>
2025-01-31 17:16:59 +0100

    Revert "CZEN-197 | feat : add AdeModal, HelModal and CourseDetailsModal"
    This reverts commit 5df6071b6192cdf7f887feb79d016dbbd778121b.

--- react-native-app/src/components/atoms/index.ts
+++ react-native-app/src/components/atoms/index.ts
@@ -2,4 +2,3 @@ export { Icon, IconProps } from './Icons';
 export { Checkbox, CheckboxProps } from './Checkbox';
 export { Text, TextProps } from './Text';
 export { Separation } from './Separation';
-
