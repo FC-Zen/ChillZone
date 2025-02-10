f5ee7c7131fa02e44616bbc64d13786a899bdb48
LoicMauritius <loicmauritius@gmail.com>
2025-02-10 17:52:59 +0100

    CZEN-255 - Line 36, Change to have multiple ALLOWED_HOSTS for the Mobile integration that connot connect with the localhost for the PC directly

--- api/chillzone/settings.py
+++ api/chillzone/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost").split(",")
 
 
 # Application definition
