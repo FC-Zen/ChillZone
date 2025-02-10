bbb73e6378aa16fb203f197486e461848d1dd650
Eliaslhl <eliaslahlouh@gmail.com>
2025-02-10 19:23:33 +0100

    CZEN-232 | feat: add zoom iOS

--- react-native-app/src/components/templates/NavigationTemplate/NavigationTemplate.tsx
+++ react-native-app/src/components/templates/NavigationTemplate/NavigationTemplate.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { View, Image, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, Platform } from 'react-native';
 import { IconWithText, NavigationBar } from '@components/molecules';
 import { styles } from './style';
 import { colors } from '@theme';
 import { useTranslation } from 'react-i18next';
+import { Zoomable } from '@likashefqet/react-native-image-zoom';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 type NavigationTemplateProps = {
   imageSource: any;
@@ -20,12 +22,20 @@ export const NavigationTemplate: React.FC<NavigationTemplateProps> = ({
 
   return (
     <View style={styles.cont}>
-      <ScrollView
-        style={styles.scrollView}
-        contentContainerStyle={styles.scrollContainer}
-      >
-        <Image source={imageSource} style={styles.image} />
-      </ScrollView>
+      <GestureHandlerRootView style={styles.zoomableContainer}>
+        <Zoomable
+          isDoubleTapEnabled
+          minScale={1}
+          maxScale={3}
+          style={styles.image}
+        >
+          <Image
+            source={imageSource}
+            style={styles.image}
+            resizeMode="contain"
+          />
+        </Zoomable>
+      </GestureHandlerRootView>
 
       <View style={styles.bottomContainer}>
         <NavigationBar
--- react-native-app/src/components/templates/NavigationTemplate/style.tsx
+++ react-native-app/src/components/templates/NavigationTemplate/style.tsx
@@ -3,9 +3,19 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   cont: {
-    backgroundColor: colors.white,
     width: '100%',
+    backgroundColor: colors.white,
     gap: 20,
+  },
+  zoomableContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: layout.window.height * 0.6,
+    gap: 10,
+    overflow: 'hidden',
+    maxHeight: layout.window.height * 0.6,
+    maxWidth: '100%',
   },
   image: {
     borderRadius: 10,
@@ -13,6 +23,13 @@ export const styles = StyleSheet.create({
     borderColor: colors.black,
     width: 350,
     height: layout.window.height * 0.6,
+    overflow: 'hidden',
+  },
+  zoomable: {
+    width: 'auto',
+    height: 'auto',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   iconContainer: {
     width: 'auto',
