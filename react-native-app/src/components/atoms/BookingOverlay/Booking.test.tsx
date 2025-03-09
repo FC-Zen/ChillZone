d517be8de5a91428ce0ea12b285eba043bc9cdda
tahathierrysefoudine <tahathierry130@gmail.com>
2025-03-09 11:03:59 +0100

    CZEN-228-bis | fix : add tests

--- react-native-app/src/components/atoms/BookingOverlay/Booking.test.tsx
+++ react-native-app/src/components/atoms/BookingOverlay/Booking.test.tsx
@@ -3,11 +3,6 @@ import { render } from '@testing-library/react-native';
 import { BookingOverlay } from './BookingOverlay';
 
 describe('BookingOverlay Component', () => {
-  it('renders component', () => {
-    const { getByText } = render(<BookingOverlay />);
-    expect(getByText('Booking Overlay')).toBeTruthy();
-  });
-
   it('transforms reservations', async () => {
     const transformedReservations = await transformReservations();
     expect(transformedReservations).toBeInstanceOf(Array);
